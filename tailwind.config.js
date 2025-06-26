cc1318f1d7500cf35437b9ed62ec88c90d3c7f08
Lander Guevarra <landerguevarra.blink@gmail.com>
2025-06-26 14:23:23 +0800

    responsiveness and load up animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ['"Mona Sans"', "ui-sans-serif", "system-ui"],
+      },
+      keyframes: {
+        "fade-in-up": {
+          "0%": { opacity: "0", transform: "translateY(40px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in-up": "fade-in-up 0.8s ease-out forwards",
       },
     },
   },
